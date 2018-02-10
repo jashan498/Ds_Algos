4984e9d8878b296bd6afd3dc786f2eda18b4ca8c
jashan498 <jashansingh.4398@gmail.com>
2018-02-10 17:48:40 +0530

    added code to print and create trees

--- Data_Structures/Trees/TreeUse.cpp
+++ Data_Structures/Trees/TreeUse.cpp
@@ -2,12 +2,64 @@
 #include "TreeNode.h"
 using namespace std;
 
-int main(){
-	TreeNode<int> * root = new TreeNode<int>(1);
-	TreeNode<int> * node1 = new TreeNode<int>(2);
-	TreeNode<int> * node2 = new TreeNode<int>(3);
+// Function to print a tree. Output for:
+//               1
+//              / \
+//             2   3
+//            /\ 
+//           4  5
+// 1 : 2, 3, 
+// 2 : 4, 5, 
+// 4 : 
+// 5 : 
+// 3 :
+void printTree(TreeNode<int> * root){
 
-	root -> children.push_back(node1);
-	root -> children.push_back(node2);
+	if(root == NULL){
+		return;
+	}
+
+	cout << root -> data << " : ";
+	for(int i=0; i<root->children.size(); i++){
+		cout<<root->children[i]->data<<", ";
+	}
+
+	cout<<endl;
+	for(int i=0; i<root->children.size(); i++){
+		printTree(root->children[i]);
+	}
+}
+
+// Function for making a tree
+TreeNode<int>* takeInput(){
+	int rootData;
+	cout<<"Enter  Data"<<endl;
+	cin>>rootData;
+
+	TreeNode<int> * root =new TreeNode<int>(rootData);
+	int n;
+	cout<<"Enter Number of children of "<<rootData<<endl;
+	cin>>n;
+
+	for(int i=0;i<n;i++){
+		TreeNode<int> * child = takeInput();
+		root->children.push_back(child);
+	}
+
+	return root;
+}
+
+int main(){
+
+	// Example for making tree manually
+	// TreeNode<int> * root = new TreeNode<int>(1);
+	// TreeNode<int> * node1 = new TreeNode<int>(2);
+	// TreeNode<int> * node2 = new TreeNode<int>(3);
+
+	// root -> children.push_back(node1);
+	// root -> children.push_back(node2);
+	
+	TreeNode<int> * root = takeInput();
+	printTree(root);
 	return 0;
 }  
